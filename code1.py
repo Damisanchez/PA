4bfc6728224679ca5e36021afa278e6687388717
Damisanchez <159930639+Damisanchez@users.noreply.github.com>
2024-03-12 00:55:02 -0500

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,12 @@
+def tree_n_plus1(numero):
+    i=0
+    while (n=!0):
+        if numero % 2 == 0:
+            n = numero / 2
+            else n = ( 3 * numero ) + 1
+            i = i + 1
+        if n = 0:
+            print.("la cantidad de veces " + i )
+            else 
+            print.(n)
+    numero = int(input("numero: ")
